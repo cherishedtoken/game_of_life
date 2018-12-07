d024e84807e9c67e069bf34a0de519821d40459e
Chris Gonzalez <twelfthapostrophe@gmail.com>
2018-12-07 08:54:39 -0800

    Simplified extinction event code. Deleted trailing whitespace.

--- game_of_life.c
+++ game_of_life.c
@@ -8,7 +8,7 @@
 // Return value when a cell lies outside the grid.
 #define OUT_OF_BOUNDS 0
 // If a cell remains the same for STABLE_CYCLES cycles, flip its value.
-#define STABLE_CYCLES 20 
+#define STABLE_CYCLES 20
 
 int getNorthNeighbor(int** gameboard, int x, int y) {
     if (y == 0) {
@@ -55,7 +55,7 @@ void print_gameboard(int** gameboard) {
         printf("\n");
     }
 
-    printf("\n"); // Separate the different instances of the board 
+    printf("\n"); // Separate the different instances of the board
 }
 
 int** create_gameboard() {
@@ -63,7 +63,7 @@ int** create_gameboard() {
 
     gameboard = malloc(GAMEBOARD_SIZE * sizeof(int*));
     if (!gameboard) abort();
-    
+
     for (int i = 0; i < GAMEBOARD_SIZE; i++) {
         gameboard[i] = malloc(GAMEBOARD_SIZE * sizeof(int));
         if (!gameboard[i]) abort();
@@ -75,7 +75,7 @@ int** create_gameboard() {
 void initialize_gameboard(int** gameboard) {
     // Now randomly decide which elements should be given life.
     srand(time(NULL));
-    
+
     for (int i = 0; i < GAMEBOARD_SIZE; i++) {
         for (int j = 0; j < GAMEBOARD_SIZE; j++) {
             gameboard[i][j] = rand() % 2;
@@ -97,11 +97,11 @@ int main() {
     int** gameboard_history = create_gameboard();
     for (int i = 0; i < GAMEBOARD_SIZE; i++) {
         for (int j = 0; j < GAMEBOARD_SIZE; j++) {
-            gameboard_history[i][j] = gameboard[i][j];
+            gameboard_history[i][j] = 0;
         }
     }
 
-    /* Here are the rules of the game: 
+    /* Here are the rules of the game:
        1. Any live cell with fewer than two live neighbors dies, as if caused by
        under-population.
        2. Any live cell with two or three live neighbors lives on to the next
@@ -109,9 +109,9 @@ int main() {
        3. Any live cell with more than three live neighbors dies, as if by
        over-population.
        4. Any dead cell with exactly three live neighbors becomes a live cell,
-       as if by reproduction. 
+       as if by reproduction.
     */
-    
+
     while (1) {
         int** gameboard_copy = create_gameboard();
         for (int i = 0; i < GAMEBOARD_SIZE; i++) {
@@ -120,24 +120,7 @@ int main() {
             }
         }
 
-        // If a particular cell remains the same for n turns, flip its value.
-        // This is done to make things more... interesting.
-        for (int i = 0; i < GAMEBOARD_SIZE; i++) {
-            for (int j = 0; j < GAMEBOARD_SIZE; j++) {
-                if (gameboard_history[i][j] >= STABLE_CYCLES) {
-                    gameboard_copy[i][j] = 0;
-                } else if (gameboard_history[i][j] <= -STABLE_CYCLES) {
-                    gameboard_copy[i][j] = 1;
-                } else if (gameboard_copy[i][j] == 1 &&
-                    gameboard_history[i][j] >= gameboard_copy[i][j]) {
-                    gameboard_history[i][j]++;
-                } else if (!gameboard_copy[i][j] &&
-                           gameboard_history[i][j] <= gameboard_copy[i][j]) {
-                    gameboard_history[i][j]--;
-                }
-            }
-        }           
-        
+
         for (int i = 0; i < GAMEBOARD_SIZE; i++) {
             for (int j = 0; j < GAMEBOARD_SIZE; j++) {
                 int neighbors = getNeighbors(gameboard_copy, i, j);
@@ -149,6 +132,19 @@ int main() {
                     } else if (neighbors == 2 || neighbors == 3) {
                         continue;
                     }
+                }
+            }
+        }
+
+        // If a particular cell remains the same for n turns, flip its value.
+        // This is done to make things more... interesting.
+        for (int i = 0; i < GAMEBOARD_SIZE; i++) {
+            for (int j = 0; j < GAMEBOARD_SIZE; j++) {
+                if (gameboard_history[i][j] >= STABLE_CYCLES) {
+                    gameboard[i][j] = !gameboard[i][j];
+                    gameboard_history[i][j] = 0;
+                } else {
+                    gameboard_history[i][j]++;
                 }
             }
         }
